9592b74a50158b536ba2d5e11d90ce4fcb906800
Eaven <zyfheaven@163.com>
2019-07-11 15:08:36 +0800

    删除不必要的包

--- src/main/java/com/pm/springboot_redis/controller/RedisController.java
+++ src/main/java/com/pm/springboot_redis/controller/RedisController.java
@@ -1,12 +1,8 @@
 package com.pm.springboot_redis.controller;
-
-import com.pm.springboot_redis.pojo.Person;
 import com.pm.springboot_redis.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
